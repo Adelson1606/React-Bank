7f696c7746d6df4d454346896fc0685ecb396a70
Veronika Adelson <adelson1606@gmail.com>
2020-06-14 12:56:37 +0300

    asxsax

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/bank', {useNewUrlParser: true, useUnifiedTopology: true})
 
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
 
-//     next()
-// })
+    next()
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
